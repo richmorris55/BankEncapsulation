608bfcec9d667b295bcddb9afbb7c77cf6f751ce
Richard Morris <richmorris55@yahoo.com>
2023-02-15 07:07:10 -0800

    InitialCommit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+
+
+		}
+
+
+			private double _balance = 0;
+
+		public void Deposit(double amount)
+		{
+
+			_balance += amount;
+
+		}
+
+
+		public void Balance()
+		{
+
+			Console.WriteLine($"The new balance is {_balance} dollars.");
+
+
+		}
+
+
+        public void Withdraw(double amount)
+        {
+
+            _balance -= amount;
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+
+           BankAccount yourBankAccount = new BankAccount();
+
+
+
+           Console.WriteLine("Please enter the amount you would like to deposit.");
+
+           var userInput = double.Parse(Console.ReadLine());
+
+            yourBankAccount.Deposit(userInput);
+
+            yourBankAccount.Balance();
+
+            Console.WriteLine("Would you like to make another deposit? Enter 1 for yes or 2 for no.");
+
+            var userInput2 = double.Parse(Console.ReadLine());
+
+            if (userInput2 == 1)
+            {
+                Console.WriteLine("Please enter the amount you would like to deposit.");
+
+                var userInput3 = double.Parse(Console.ReadLine());
+
+                yourBankAccount.Deposit(userInput3);
+
+                yourBankAccount.Balance();
+            }
+
+
         }
     }
 }
